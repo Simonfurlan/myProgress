8ca80e278e28927f84a298d7938eb240c95c1913
Vorname Nachname <deine.email@TFO.it>
2023-07-25 11:27:03 +0200

    Edit button

--- script.js
+++ script.js
@@ -1,19 +1,51 @@
 function expandContract(day) {
-   
+  if(event.target.id == "container" || event.target.id == "bottom-section" || event.target.id == "title" || event.target.id == "titleName"){
     switch(day) {
-        case 'm':
-            const mon = document.getElementById("expand-contract-mon")
-            mon.classList.toggle('expanded')
-            mon.classList.toggle('collapsed')
-          break;
-        case 't':
-            const tue = document.getElementById("expand-contract-tue")
-            tue.classList.toggle('expanded')
-            tue.classList.toggle('collapsed')
-          break;
-        default:
-          alert("Error identifying Day")
+            case 'm':
+                const mon = document.getElementById("expand-contract-mon")
+                const btnMon = document.getElementById("btnMon")
+                mon.classList.toggle('expanded')
+                mon.classList.toggle('collapsed')
+                
+                if(mon.classList.contains('expanded')){
+                  setTimeout(() => {btnMon.style.visibility = "visible";}, 250);
+                }
+                else{
+                  btnMon.style.visibility = "hidden";
+                  btnMon.className = "btnEdit";
+                }
+                
+              break;
+            case 't':
+                const tue = document.getElementById("expand-contract-tue")
+                const btnTue = document.getElementById("btnTue")
+                tue.classList.toggle('expanded')
+                tue.classList.toggle('collapsed')
+
+                if(tue.classList.contains('expanded')){
+                  setTimeout(() => {btnTue.style.visibility = "visible";}, 250);
+                }
+                else{
+                  btnTue.style.visibility = "hidden";
+                  btnTue.className = "btnEdit";
+                }
+              break;
+            default:
+              alert("Error identifying Day")
       }
+  }
+}
+
+function buttonMonday() {
+  const btnMon = document.getElementById("btnMon");
+  btnMon.classList.toggle('btnAdd');
+  btnMon.classList.toggle('btnEdit');
+}
+
+function buttonTuesday() {
+  const btnTue = document.getElementById("btnTue");
+  btnTue.classList.toggle('btnAdd');
+  btnTue.classList.toggle('btnEdit');
 }
 
 function addToTable(table, e1, e2, e3, e4) {
