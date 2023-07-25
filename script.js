f4a523f9360991119be35239a4e7be43066111a8
Vorname Nachname <deine.email@TFO.it>
2023-07-25 08:27:37 +0200

    Table insertion

--- script.js
+++ script.js
@@ -15,3 +15,22 @@ function expandContract(day) {
           alert("Error identifying Day")
       }
 }
+
+function addToTable(table, e1, e2, e3, e4) {
+
+  let tb = document.getElementById(table);
+
+  let row = tb.insertRow(-1); 
+
+  let c1 = row.insertCell(0);
+  let c2 = row.insertCell(1);
+  let c3 = row.insertCell(2);
+  let c4 = row.insertCell(3);
+
+  c1.innerText = e1
+  c2.innerText = e2
+  c3.innerText = e3
+  c4.innerText = e4
+}
+
+addToTable("tableMon", "Calf Raises", "150kg", "6", "15")
