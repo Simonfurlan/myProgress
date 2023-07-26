c3c4538b2bb6419e716fab8de903c9a8a2c8ac8a
Vorname Nachname <deine.email@TFO.it>
2023-07-26 18:15:22 +0200

    Add days

--- script.js
+++ script.js
@@ -3,111 +3,6 @@ let selectedIndex = -1;
 
 loadStorage()
 
-function loadStorage(){
-  tables = ["tableMon", "tableTue"]
-
-  for (let t = 0; t < tables.length; t++) {
-    for (let i = 0; localStorage.getItem(tables[t] + i) != null; i++) {
-      e = localStorage.getItem(tables[t] + i).split('$');
-      let tb = document.getElementById(tables[t]);
-      let row = tb.insertRow(-1); 
-      let c1 = row.insertCell(0);
-      let c2 = row.insertCell(1);
-      let c3 = row.insertCell(2);
-      let c4 = row.insertCell(3);
-      c1.innerText = e[0]
-      c2.innerText = e[1]
-      c3.innerText = e[2]
-      c4.innerText = e[3]
-    }
-
-    if(localStorage.getItem(tables[t] + "0") == null){
-      document.getElementById(tables[t]).style.visibility = "collapse";
-      document.getElementById(tables[t]).style.display = "none";
-      console.log("btn" + (tables[t].slice(5)));
-      document.getElementById("btn" + (tables[t].slice(5))).className = 'btnAdd';
-      document.getElementById("hint" + (tables[t].slice(5))).style.display = 'inherit';
-    }
-  }
-  calculateVolume("Mon");
-  calculateVolume("Tue");
-}
-
-function calculateVolume(day){
-  let sum = 0;
-  for (let i = 0; localStorage.getItem("table"+ day + i) != null; i++) {
-    e = localStorage.getItem("table"+ day + i).split('$');
-    
-    for (let j = 0; j < parseInt(e[2]); j++){
-      if((parseInt(e[1]) - (parseInt(e[3]) * j)) > 0){
-        sum += (parseInt(e[1]) - (parseInt(e[3]) * j)) * 12;
-      }
-      else{
-        //alert("Weight to Increment ratio to high: " + e[0]);
-      }
-    }
-  }
-  if(sum > 0){
-    document.getElementById("volume" + day).innerHTML = "Total Volume: " + sum + "kg";
-    document.getElementById("container" + day).style.backgroundImage = "linear-gradient(#ffd60a, #ffcc00)";
-  }
-  else{
-    document.getElementById("volume" + day).innerHTML = "Looks like a Rest Day";
-  }
-}
-
-function expandContract(day) {
-  if(event.target.id == "container" || event.target.id == "bottom-section" || event.target.id == "title" || event.target.id == "titleName"){
-    switch(day) {
-            case 'm':
-                const mon = document.getElementById("expand-contract-mon")
-                const btnMon = document.getElementById("btnMon")
-                mon.classList.toggle('expanded')
-                mon.classList.toggle('collapsed')
-                
-                if(mon.classList.contains('expanded')){
-                    btnMon.style.visibility = "visible";
-                    btnMon.style.opacity = 1;
-                    btnMon.style.transition = "opacity 0.25s linear";
-
-                    if(localStorage.getItem("tableMon0") == null){
-                      document.getElementById("btnMon").className = 'btnAdd';
-                    }
-                }
-                else{
-                  btnMon.style.visibility = "hidden";
-                  btnMon.style.opacity = 0;
-                  btnMon.className = "btnEdit";
-                }
-                
-              break;
-            case 't':
-                const tue = document.getElementById("expand-contract-tue")
-                const btnTue = document.getElementById("btnTue")
-                tue.classList.toggle('expanded')
-                tue.classList.toggle('collapsed')
-
-                if(tue.classList.contains('expanded')){
-                  btnTue.style.visibility = "visible";
-                  btnTue.style.opacity = 1;
-                  btnTue.style.transition = "opacity 0.25s linear";
-
-                  if(localStorage.getItem("tableTue0") == null){
-                    document.getElementById("btnTue").className = 'btnAdd';
-                  }
-                }
-                else{
-                  btnTue.style.visibility = "hidden";
-                  btnTue.style.opacity = 0;
-                  btnTue.className = "btnEdit";
-                }
-              break;
-            default:
-              alert("Error identifying Day")
-      }
-  }
-}
-
 function buttonMonday() {
   const btnMon = document.getElementById("btnMon");
   btnMon.classList.toggle('btnAdd');
@@ -133,6 +28,91 @@ function buttonTuesday() {
   if(btnTue.classList.contains('btnEdit')){
     //Case pressed on Add
     selectedDay = "Tue";
+    showPopup();
+    //Remove old inputs
+    document.getElementById("input1").value = "";
+    document.getElementById("input2").value = "";
+    document.getElementById("input3").value = "";
+    document.getElementById("input4").value = "";
+  }
+}
+
+function buttonWednesday() {
+  const btnWed = document.getElementById("btnWed");
+  btnWed.classList.toggle('btnAdd');
+  btnWed.classList.toggle('btnEdit');
+
+  if(btnWed.classList.contains('btnEdit')){
+    //Case pressed on Add
+    selectedDay = "Wed";
+    showPopup();
+    //Remove old inputs
+    document.getElementById("input1").value = "";
+    document.getElementById("input2").value = "";
+    document.getElementById("input3").value = "";
+    document.getElementById("input4").value = "";
+  }
+}
+
+function buttonThursday() {
+  const btnThu = document.getElementById("btnThu");
+  btnThu.classList.toggle('btnAdd');
+  btnThu.classList.toggle('btnEdit');
+
+  if(btnThu.classList.contains('btnEdit')){
+    //Case pressed on Add
+    selectedDay = "Thu";
+    showPopup();
+    //Remove old inputs
+    document.getElementById("input1").value = "";
+    document.getElementById("input2").value = "";
+    document.getElementById("input3").value = "";
+    document.getElementById("input4").value = "";
+  }
+}
+
+function buttonFriday() {
+  const btnFri = document.getElementById("btnFri");
+  btnFri.classList.toggle('btnAdd');
+  btnFri.classList.toggle('btnEdit');
+
+  if(btnFri.classList.contains('btnEdit')){
+    //Case pressed on Add
+    selectedDay = "Fri";
+    showPopup();
+    //Remove old inputs
+    document.getElementById("input1").value = "";
+    document.getElementById("input2").value = "";
+    document.getElementById("input3").value = "";
+    document.getElementById("input4").value = "";
+  }
+}
+
+function buttonSaturday() {
+  const btnSat = document.getElementById("btnSat");
+  btnSat.classList.toggle('btnAdd');
+  btnSat.classList.toggle('btnEdit');
+
+  if(btnSat.classList.contains('btnEdit')){
+    //Case pressed on Add
+    selectedDay = "Sat";
+    showPopup();
+    //Remove old inputs
+    document.getElementById("input1").value = "";
+    document.getElementById("input2").value = "";
+    document.getElementById("input3").value = "";
+    document.getElementById("input4").value = "";
+  }
+}
+
+function buttonSunday() {
+  const btnSun = document.getElementById("btnSun");
+  btnSun.classList.toggle('btnAdd');
+  btnSun.classList.toggle('btnEdit');
+
+  if(btnSun.classList.contains('btnEdit')){
+    //Case pressed on Add
+    selectedDay = "Sun";
     showPopup();
     //Remove old inputs
     document.getElementById("input1").value = "";
@@ -206,7 +186,162 @@ function clickTableTue(){
   }
 }
 
+function clickTableWed(){
+  if(btnWed.classList.contains('btnAdd')){
+    //Case pressed on Edit
+    if(isNaN((event.target.parentNode.rowIndex)) || event.target.parentNode.rowIndex == 0){
+      return;
+    }
 
+    let index = event.target.parentNode.rowIndex;
+    selectedIndex = index;
+    selectedDay = "Wed";
+    showPopup();
+
+    const tableWed = document.getElementById("tableWed");
+    const input1 = document.getElementById("input1");
+    const input2 = document.getElementById("input2");
+    const input3 = document.getElementById("input3");
+    const input4 = document.getElementById("input4");
+
+    elements = tableWed.rows[index].cells;
+    e1 = elements[0].innerText;
+    e2 = elements[1].innerText;
+    e3 = elements[2].innerText;
+    e4 = elements[3].innerText;
+
+    input1.value = e1;
+    input2.value = e2;
+    input3.value = e3;
+    input4.value = e4;
+  }
+}
+
+function clickTableThu(){
+  if(btnThu.classList.contains('btnAdd')){
+    //Case pressed on Edit
+    if(isNaN((event.target.parentNode.rowIndex)) || event.target.parentNode.rowIndex == 0){
+      return;
+    }
+
+    let index = event.target.parentNode.rowIndex;
+    selectedIndex = index;
+    selectedDay = "Thu";
+    showPopup();
+
+    const tableThu = document.getElementById("tableThu");
+    const input1 = document.getElementById("input1");
+    const input2 = document.getElementById("input2");
+    const input3 = document.getElementById("input3");
+    const input4 = document.getElementById("input4");
+
+    elements = tableThu.rows[index].cells;
+    e1 = elements[0].innerText;
+    e2 = elements[1].innerText;
+    e3 = elements[2].innerText;
+    e4 = elements[3].innerText;
+
+    input1.value = e1;
+    input2.value = e2;
+    input3.value = e3;
+    input4.value = e4;
+  }
+}
+
+function clickTableFri(){
+  if(btnFri.classList.contains('btnAdd')){
+    //Case pressed on Edit
+    if(isNaN((event.target.parentNode.rowIndex)) || event.target.parentNode.rowIndex == 0){
+      return;
+    }
+
+    let index = event.target.parentNode.rowIndex;
+    selectedIndex = index;
+    selectedDay = "Fri";
+    showPopup();
+
+    const tableFri = document.getElementById("tableFri");
+    const input1 = document.getElementById("input1");
+    const input2 = document.getElementById("input2");
+    const input3 = document.getElementById("input3");
+    const input4 = document.getElementById("input4");
+
+    elements = tableFri.rows[index].cells;
+    e1 = elements[0].innerText;
+    e2 = elements[1].innerText;
+    e3 = elements[2].innerText;
+    e4 = elements[3].innerText;
+
+    input1.value = e1;
+    input2.value = e2;
+    input3.value = e3;
+    input4.value = e4;
+  }
+}
+
+function clickTableSat(){
+  if(btnSat.classList.contains('btnAdd')){
+    //Case pressed on Edit
+    if(isNaN((event.target.parentNode.rowIndex)) || event.target.parentNode.rowIndex == 0){
+      return;
+    }
+
+    let index = event.target.parentNode.rowIndex;
+    selectedIndex = index;
+    selectedDay = "Sat";
+    showPopup();
+
+    const tableSat = document.getElementById("tableSat");
+    const input1 = document.getElementById("input1");
+    const input2 = document.getElementById("input2");
+    const input3 = document.getElementById("input3");
+    const input4 = document.getElementById("input4");
+
+    elements = tableSat.rows[index].cells;
+    e1 = elements[0].innerText;
+    e2 = elements[1].innerText;
+    e3 = elements[2].innerText;
+    e4 = elements[3].innerText;
+
+    input1.value = e1;
+    input2.value = e2;
+    input3.value = e3;
+    input4.value = e4;
+  }
+}
+
+function clickTableSun(){
+  if(btnSun.classList.contains('btnAdd')){
+    //Case pressed on Edit
+    if(isNaN((event.target.parentNode.rowIndex)) || event.target.parentNode.rowIndex == 0){
+      return;
+    }
+
+    let index = event.target.parentNode.rowIndex;
+    selectedIndex = index;
+    selectedDay = "Sun";
+    showPopup();
+
+    const tableSun = document.getElementById("tableSun");
+    const input1 = document.getElementById("input1");
+    const input2 = document.getElementById("input2");
+    const input3 = document.getElementById("input3");
+    const input4 = document.getElementById("input4");
+
+    elements = tableSun.rows[index].cells;
+    e1 = elements[0].innerText;
+    e2 = elements[1].innerText;
+    e3 = elements[2].innerText;
+    e4 = elements[3].innerText;
+
+    input1.value = e1;
+    input2.value = e2;
+    input3.value = e3;
+    input4.value = e4;
+  }
+}
+
+//Shared
 
 function addToTable(table, e1, e2, e3, e4) {
 
@@ -279,4 +414,213 @@ function processInputs() {
       }
       closePopup();
   }
+}
+
+function calculateVolume(day){
+  let sum = 0;
+  for (let i = 0; localStorage.getItem("table"+ day + i) != null; i++) {
+    e = localStorage.getItem("table"+ day + i).split('$');
+    sum += (parseInt(e[1]) * (parseInt(e[3])));
+  }
+  if(sum > 0){
+    document.getElementById("volume" + day).innerHTML = "Max Set Volume: " + sum + "kg";
+    document.getElementById("container" + day).style.backgroundImage = "linear-gradient(#ffd60a, #ffcc00)";
+  }
+  else{
+    document.getElementById("volume" + day).innerHTML = "Looks like a Rest Day";
+  }
+}
+
+function expandContract(day) {
+  if(event.target.id == "container" || event.target.id == "bottom-section" || event.target.id == "title" || event.target.id == "titleName"){
+    switch(day) {
+            case 'm':
+                const mon = document.getElementById("expand-contract-mon")
+                const btnMon = document.getElementById("btnMon")
+                mon.classList.toggle('expanded')
+                mon.classList.toggle('collapsed')
+                
+                if(mon.classList.contains('expanded')){
+                    btnMon.style.visibility = "visible";
+                    btnMon.style.opacity = 1;
+                    btnMon.style.transition = "opacity 0.25s linear";
+
+                    if(localStorage.getItem("tableMon0") == null){
+                      document.getElementById("btnMon").className = 'btnAdd';
+                    }
+                }
+                else{
+                  btnMon.style.visibility = "hidden";
+                  btnMon.style.opacity = 0;
+                  btnMon.className = "btnEdit";
+                }
+                
+              break;
+
+            case 't':
+                const tue = document.getElementById("expand-contract-tue")
+                const btnTue = document.getElementById("btnTue")
+                tue.classList.toggle('expanded')
+                tue.classList.toggle('collapsed')
+
+                if(tue.classList.contains('expanded')){
+                  btnTue.style.visibility = "visible";
+                  btnTue.style.opacity = 1;
+                  btnTue.style.transition = "opacity 0.25s linear";
+
+                  if(localStorage.getItem("tableTue0") == null){
+                    document.getElementById("btnTue").className = 'btnAdd';
+                  }
+                }
+                else{
+                  btnTue.style.visibility = "hidden";
+                  btnTue.style.opacity = 0;
+                  btnTue.className = "btnEdit";
+                }
+              break;
+
+              case 'w':
+                const wed = document.getElementById("expand-contract-wed")
+                const btnWed = document.getElementById("btnWed")
+                wed.classList.toggle('expanded')
+                wed.classList.toggle('collapsed')
+
+                if(wed.classList.contains('expanded')){
+                  btnWed.style.visibility = "visible";
+                  btnWed.style.opacity = 1;
+                  btnWed.style.transition = "opacity 0.25s linear";
+
+                  if(localStorage.getItem("tableWed0") == null){
+                    document.getElementById("btnWed").className = 'btnAdd';
+                  }
+                }
+                else{
+                  btnWed.style.visibility = "hidden";
+                  btnWed.style.opacity = 0;
+                  btnWed.className = "btnEdit";
+                }
+              break;
+
+              case 'u':
+                const thu = document.getElementById("expand-contract-thu")
+                const btnThu = document.getElementById("btnThu")
+                thu.classList.toggle('expanded')
+                thu.classList.toggle('collapsed')
+
+                if(thu.classList.contains('expanded')){
+                  btnThu.style.visibility = "visible";
+                  btnThu.style.opacity = 1;
+                  btnThu.style.transition = "opacity 0.25s linear";
+
+                  if(localStorage.getItem("tableThu0") == null){
+                    document.getElementById("btnThu").className = 'btnAdd';
+                  }
+                }
+                else{
+                  btnThu.style.visibility = "hidden";
+                  btnThu.style.opacity = 0;
+                  btnThu.className = "btnEdit";
+                }
+              break;
+
+              case 'f':
+                const fri = document.getElementById("expand-contract-fri")
+                const btnFri = document.getElementById("btnFri")
+                fri.classList.toggle('expanded')
+                fri.classList.toggle('collapsed')
+
+                if(fri.classList.contains('expanded')){
+                  btnFri.style.visibility = "visible";
+                  btnFri.style.opacity = 1;
+                  btnFri.style.transition = "opacity 0.25s linear";
+
+                  if(localStorage.getItem("tableFri0") == null){
+                    document.getElementById("btnFri").className = 'btnAdd';
+                  }
+                }
+                else{
+                  btnFri.style.visibility = "hidden";
+                  btnFri.style.opacity = 0;
+                  btnFri.className = "btnEdit";
+                }
+              break;
+
+              case 's':
+                const sat = document.getElementById("expand-contract-sat")
+                const btnSat = document.getElementById("btnSat")
+                sat.classList.toggle('expanded')
+                sat.classList.toggle('collapsed')
+
+                if(sat.classList.contains('expanded')){
+                  btnSat.style.visibility = "visible";
+                  btnSat.style.opacity = 1;
+                  btnSat.style.transition = "opacity 0.25s linear";
+
+                  if(localStorage.getItem("tableSat0") == null){
+                    document.getElementById("btnSat").className = 'btnAdd';
+                  }
+                }
+                else{
+                  btnSat.style.visibility = "hidden";
+                  btnSat.style.opacity = 0;
+                  btnSat.className = "btnEdit";
+                }
+              break;
+
+              case 'S':
+                const sun = document.getElementById("expand-contract-sun")
+                const btnSun = document.getElementById("btnSun")
+                sun.classList.toggle('expanded')
+                sun.classList.toggle('collapsed')
+
+                if(sun.classList.contains('expanded')){
+                  btnSun.style.visibility = "visible";
+                  btnSun.style.opacity = 1;
+                  btnSun.style.transition = "opacity 0.25s linear";
+
+                  if(localStorage.getItem("tableSun0") == null){
+                    document.getElementById("btnSun").className = 'btnAdd';
+                  }
+                }
+                else{
+                  btnSun.style.visibility = "hidden";
+                  btnSun.style.opacity = 0;
+                  btnSun.className = "btnEdit";
+                }
+              break;
+
+            default:
+              alert("Error identifying Day")
+      }
+  }
+}
+
+function loadStorage(){
+  tables = ["tableMon", "tableTue", "tableWed", "tableThu", "tableFri", "tableSat", "tableSun"]
+
+  for (let t = 0; t < tables.length; t++) {
+    for (let i = 0; localStorage.getItem(tables[t] + i) != null; i++) {
+      e = localStorage.getItem(tables[t] + i).split('$');
+      let tb = document.getElementById(tables[t]);
+      let row = tb.insertRow(-1); 
+      let c1 = row.insertCell(0);
+      let c2 = row.insertCell(1);
+      let c3 = row.insertCell(2);
+      let c4 = row.insertCell(3);
+      c1.innerText = e[0]
+      c2.innerText = e[1]
+      c3.innerText = e[2]
+      c4.innerText = e[3]
+    }
+
+    if(localStorage.getItem(tables[t] + "0") == null){
+      document.getElementById(tables[t]).style.visibility = "collapse";
+      document.getElementById(tables[t]).style.display = "none";
+      console.log("btn" + (tables[t].slice(5)));
+      document.getElementById("btn" + (tables[t].slice(5))).className = 'btnAdd';
+      document.getElementById("hint" + (tables[t].slice(5))).style.display = 'inherit';
+    }
+  }
+  calculateVolume("Mon");
+  calculateVolume("Tue");
 }
